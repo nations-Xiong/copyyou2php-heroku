9d49ac81c5d7f9961b03bec2f5e99cb940d1c360
nations-Xiong <57881922+nations-Xiong@users.noreply.github.com>
2020-07-01 07:45:14 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');//这里改成您的api key
+define('APIKEY', 'AIzaSyDWgpoG0qsJJZiNUUxRNZ1_V4wScL_sAI0');//这里改成您的api key
 define('GJ_CODE', 'NZ');//国家代码
-define('SITE_NAME', 'Share2018');//网站名字
-define('TITLENAME', '共享網飛群&遊管');//网站名字
+define('SITE_NAME', 'Youtube_fork');//网站名字
+define('TITLENAME', 'Yourtube_fork');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'wingwanwin@gmail.com');//邮箱
+define('EMAIL', 'nations19991117@outlook.com');//邮箱
 ?>
